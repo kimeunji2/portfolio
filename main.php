d86838a65d27a1726c5d7c2aef66d0c40251b97b
kimeunji2 <81205131+kimeunji2@users.noreply.github.com>
2021-04-30 14:04:43 +0900

    Add files via upload

---
+++ main.php
@@ -0,0 +1,312 @@
+<? include $_SERVER["DOCUMENT_ROOT"]."/portfolio/head_lib.php";  ?>
+<? include $_SERVER["DOCUMENT_ROOT"]."/portfolio/nav.php";  ?>
+
+
+<div id="content">
+	<div class="portfolio">
+		<ul class="pf_list">
+			<!-- <li class="sunflower">
+				<div class="site_name">
+					타워프로미스<span class="eng">tower promise</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							타워프로미스<span class="eng">tower promise</span>
+						</div>
+						<a class="view" href="/portfolio/work/towerp.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li> -->
+			<li class="wjcnc">
+				<div class="site_name">
+					우진 씨앤씨<span class="eng">woojin cnc</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							우진 씨앤씨<span class="eng">woojin cnc</span>
+						</div>
+						<a class="view" href="/portfolio/work/wjcnc.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li>
+			<!-- <li class="shinalaw">
+				<div class="site_name">
+					신아법무사사무소<span class="eng">shina law</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							신아법무사사무소<span class="eng">shina law</span>
+						</div>
+						<a class="view" href="/portfolio/work/shinalaw.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li> -->
+			<li class="jobsich">
+				<div class="site_name">
+					잡스 인천<span class="eng">jobs incheon</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							잡스 인천<span class="eng">jobs incheon</span>
+						</div>
+						<a class="view" href="/portfolio/work/jobsich.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="hk">
+				<div class="site_name">
+					HK해외금융<span class="eng">hk investing</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							HK해외금융<span class="eng">hk investing</span>
+						</div>
+						<a class="view" href="/portfolio/work/hk.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="ecowood">
+				<div class="site_name">
+					에코우드<span class="eng">ecowood</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							에코우드<span class="eng">ecowood</span>
+						</div>
+						<a class="view" href="/portfolio/work/ecowood.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="artisan">
+				<div class="site_name">
+					청담아티젠<span class="eng">artisan clinic</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							청담아티젠<span class="eng">artisan clinic</span>
+						</div>
+						<a class="view" href="/portfolio/work/artisan.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="withjob">
+				<div class="site_name">
+					위드잡<span class="eng">with job</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							위드잡<span class="eng">with job</span>
+						</div>
+						<a class="view" href="/portfolio/work/withjob.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="vatang">
+				<div class="site_name">
+					바탕 건축<span class="eng">ARCHITECTURE DESIGN GROUP VATANG</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							바탕 건축<span class="eng">VATANG</span>
+						</div>
+						<a class="view" href="/portfolio/work/vatang.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li>
+			<!-- <li class="ansony">
+				<div class="site_name">
+					앤소니<span class="eng">ansony bebe</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							앤소니<span class="eng">ansony bebe</span>
+						</div>
+						<a class="view" href="/portfolio/work/ansony.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li> -->
+			<li class="alices">
+				<div class="site_name">
+					앨리스 가든<span class="eng">alices garden</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							앨리스 가든<span class="eng">alices garden</span>
+						</div>
+						<a class="view" href="/portfolio/work/alices.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="yumsem">
+				<div class="site_name">
+					얌샘<span class="eng">yumsem</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							얌샘<span class="eng">yumsem</span>
+						</div>
+						<a class="view" href="/portfolio/work/yumsem.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="glovane">
+				<div class="site_name">
+					글로베인<span class="eng">glovane</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							글로베인<span class="eng">glovane</span>
+						</div>
+						<a class="view" href="/portfolio/work/glovane.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="visionbp">
+				<div class="site_name">
+					비전비엔피<span class="eng">vision b&p</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							비전비엔피<span class="eng">vision b&p</span>
+						</div>
+						<a class="view" href="/portfolio/work/visionbp.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="metlife">
+				<div class="site_name">
+					메트라이프 360헬스 앱<span class="eng">Metlife 360Health app</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							메트라이프 360헬스 앱<span class="eng">Metlife 360Health app</span>
+						</div>
+						<a class="view" href="/portfolio/work/metlife.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li>
+			<!-- <li class="cnpt">
+				<div class="site_name">
+					청년피티<span class="eng">cnpt studio</span>
+				</div>
+				<div class="hover">
+					<div class="hover_in">
+						<div class="s_name">
+							청년피티<span class="eng">cnpt studio</span>
+						</div>
+						<a class="view" href="/portfolio/work/cnpt.php">
+							DETAIL VIEW
+							<div class="arrow">
+								<span class="bar"></span>
+							</div>
+						</a>
+					</div>
+				</div>
+			</li> -->
+		</ul>
+		<div class="read_more">
+			<div class="btn">
+				READ MORE
+				<div class="arrow">
+					<span class="bar"></span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="copyright">
+		<p>Copyrightⓒ 2020 . All Rights Reserved</p>
+	</div>
+</div>
+
+
+<? include $_SERVER["DOCUMENT_ROOT"]."/portfolio/foot.php";  ?>
